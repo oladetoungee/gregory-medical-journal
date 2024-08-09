7a96acc37149bf38d3f253ad0c6c342bcedb2b72
oladetoungee <demilad1998@gmail.com>
2024-08-09 10:35:08 +0100

    update

--- src/router/protected.tsx
+++
@@ -1,8 +0,0 @@
-import { RouteObject } from 'react-router-dom';
-import { __PRODUCT__ } from '@/lib/hooks/use-product';
-import { companyRoutes } from '@/features/company/router';
-import { lawyerRoutes } from '@/features/lawyer/router';
-
-export const protectedRoutes: RouteObject[] = [
-  ...(__PRODUCT__ === 'vega-company' ? companyRoutes : lawyerRoutes)
-];
--- src/router/public.tsx
+++
@@ -1,6 +0,0 @@
-import { RouteObject } from 'react-router-dom';
-import { authRoutes } from '@/features/auth/router';
-
-export const publicRoutes: RouteObject[] = [
-  ...authRoutes
-];
