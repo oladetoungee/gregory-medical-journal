87ab495c9bb904b26cc6c24af23acff6534c5b7f
oladetoungee <demilad1998@gmail.com>
2024-08-20 17:31:43 +0100

    update

--- src/components/EditorialBoard.tsx
+++ src/components/EditorialBoard.tsx
@@ -5,10 +5,14 @@ import { editorialBoard } from '@/constants';
 
 const EditorialBoard = () => {
   return (
-    <div className=" py-8 px-4 sm:px-6 lg:px-8 bg-gray-100">
+    <div className="py-12 px-4 sm:px-6 lg:px-8 bg-gray-100">
       <div className="container mx-auto">
-        <h1 className="text-3xl sm:text-4xl font-bold text-center text-primary mb-6">{editorialBoard.title}</h1>
-        <p className="text-base sm:text-lg text-gray-700 leading-7 mb-6">{editorialBoard.description}</p>
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold text-center text-primary mb-6">
+          {editorialBoard.title}
+        </h1>
+        <p className="text-xs sm:text-sm lg:text-base text-gray-700 leading-5 mb-6">
+          {editorialBoard.description}
+        </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {editorialBoard.members.map((member, index) => (
             <div key={index} className="bg-white shadow-lg rounded-lg overflow-hidden">
@@ -16,9 +20,15 @@ const EditorialBoard = () => {
                 <MemberImage name={member.name} imageSrc={member.image} />
               </div>
               <div className="p-4 text-center">
-                <h3 className="text-xl sm:text-2xl font-bold text-primary mb-1">{member.name}</h3>
-                <p className="text-sm sm:text-base text-gray-500 mb-2">{member.title}</p>
-                <p className="text-base sm:text-lg text-gray-700 leading-7">{member.bio}</p>
+                <h3 className="text-sm sm:text-base lg:text-lg font-bold text-primary mb-1">
+                  {member.name}
+                </h3>
+                <p className="text-xs sm:text-sm lg:text-base text-gray-500 mb-2">
+                  {member.title}
+                </p>
+                <p className="text-xs sm:text-sm lg:text-base text-gray-700 leading-6">
+                  {member.bio}
+                </p>
               </div>
             </div>
           ))}
@@ -43,7 +53,7 @@ const MemberImage: React.FC<MemberImageProps> = ({ name, imageSrc }) => {
   };
 
   return imageError ? (
-    <div className="w-full h-48 flex items-center justify-center bg-gray-200 text-gray-600 rounded-t-lg">
+    <div className="w-full h-48 flex items-center justify-center bg-gray-200 text-primary rounded-t-lg">
       <span className="text-4xl font-bold">{getInitials(name)}</span>
     </div>
   ) : (
