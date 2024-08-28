1473bc79731edd6ff6232f530ebefeb94025a2ae
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-08-28 01:01:52 +0100

    editorial Board

--- src/components/EditorialBoard.tsx
+++ src/components/EditorialBoard.tsx
@@ -1,65 +1,39 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
-import { editorialBoard } from "@/constants";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Button, Typewriter } from "@/components/";
 import { ArrowRightIcon } from "@radix-ui/react-icons";
+import axios from "axios";
 
 interface EditorialBoardProps {
   showMembers: boolean;
 }
 
-const EditorialBoard = ({ showMembers }: EditorialBoardProps) => {
-  return (
-    <div className="py-12 px-4 sm:px-6 lg:px-8 bg-gray-100">
-      <div className="container mx-auto">
-        <Typewriter
-          className="page-header"
-          text={editorialBoard.title}
-        ></Typewriter>
+interface ImageData {
+  data: {
+    id: number;
+    attributes: {
+      name: string;
+      alternativeText: string;
+      width: number;
+      height: number;
+      url: string;
+    };
+  };
+}
 
-        <p className="text-xs sm:text-sm lg:text-base text-gray-700 leading-5 mb-6">
-          {editorialBoard.description}
-        </p>
-        {showMembers && (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {editorialBoard.members.map((member, index) => (
-              <div
-                key={index}
-                className="bg-white shadow-lg rounded-lg overflow-hidden"
-              >
-                <div className="relative">
-                  <MemberImage name={member.name} imageSrc={member.image} />
-                </div>
-                <div className="p-4 text-center">
-                  <h3 className="text-sm sm:text-base lg:text-lg font-bold text-primary mb-1">
-                    {member.name}
-                  </h3>
-                  <p className="text-xs sm:text-sm lg:text-base text-gray-500 mb-2">
-                    {member.title}
-                  </p>
-                  <p className="text-xs sm:text-sm lg:text-base text-gray-700 leading-6">
-                    {member.bio}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-        )}
-        {!showMembers && (
-          <Link href="/about/editorial-board">
-            <Button icon={<ArrowRightIcon />}>Board Members</Button>
-          </Link>
-        )}
-      </div>
-    </div>
-  );
-};
+interface Member {
+  id: number;
+  name: string;
+  bio: string;
+  role: string;
+  image: ImageData | null;
+}
 
 type MemberImageProps = {
   name: string;
-  imageSrc: string;
+  imageSrc: string | null;
 };
 
 const MemberImage = ({ name, imageSrc }: MemberImageProps) => {
@@ -77,13 +51,100 @@ const MemberImage = ({ name, imageSrc }: MemberImageProps) => {
     </div>
   ) : (
     <Image
-      src={imageSrc}
+      src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${imageSrc}`}
       alt={name}
       layout="fill"
       objectFit="cover"
       className="w-full h-48 rounded-t-lg"
       onError={() => setImageError(true)}
     />
+  );
+};
+
+const EditorialBoard = ({ showMembers }: EditorialBoardProps) => {
+  const [members, setMembers] = useState<Member[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMembers = async () => {
+      try {
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_STRAPI_URL}/members?populate=*`);
+        console.log(response.data);
+        const member = response.data.data.map((data: any) => ({
+          id: data.id,
+          name: data.attributes.name,
+          bio: data.attributes.bio,
+          role: data.attributes.role,
+          image: data.attributes.image?.data?.attributes?.url || null,
+        }))
+
+      setMembers(member);
+      setLoading(false);
+      console.log("member:", member)
+      } catch (error) {
+        console.log('Error fetching members:', error);
+        setLoading(false); 
+      }
+    }
+      fetchMembers();
+  }, []);
+  
+  if (loading) {
+    return <div className="mt-10">Loading...</div>;
+  }
+
+  return (
+    <div className="py-12 px-4 sm:px-6 lg:px-8 bg-gray-100">
+      <div className="container mx-auto">
+        <Typewriter
+          className="page-header h-12"
+          text="Editorial Board"
+        ></Typewriter>
+
+        <p className="text-xs sm:text-sm lg:text-base text-gray-700 leading-5 mb-6">
+          Our editorial board comprises esteemed professionals and academics with extensive expertise 
+          in their respective fields. They play a crucial role in ensuring the quality and integrity of 
+          the articles published in Gregory Medical Journal.
+        </p>
+        {showMembers && (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {members.map((member) => (
+              <div
+                key={member.id}
+                className="bg-white shadow-lg rounded-lg overflow-hidden"
+              >
+                <div className="relative">
+                  <MemberImage
+                    name={member.name}
+                    imageSrc={
+                      member.image
+                        ? `${process.env.NEXT_PUBLIC_STRAPI_URL}${member.image}`
+                        : null
+                    }
+                  />
+                </div>
+                <div className="p-4 text-center">
+                  <h3 className="text-sm sm:text-base lg:text-lg font-bold text-primary mb-1">
+                    {member.name}
+                  </h3>
+                  <p className="text-xs sm:text-sm lg:text-base text-gray-500 mb-2">
+                    {member.role}
+                  </p>
+                  <p className="text-xs sm:text-sm lg:text-base text-gray-700 leading-6">
+                    {member.bio}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {!showMembers && (
+          <Link href="/about/editorial-board">
+            <Button icon={<ArrowRightIcon />}>Board Members</Button>
+          </Link>
+        )}
+      </div>
+    </div>
   );
 };
 
