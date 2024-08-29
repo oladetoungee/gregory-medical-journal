e47167900ca3fdd634d71e71a512a31337dec5f3
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-08-29 21:36:36 +0100

    rest of the data

--- src/components/ArticlesList.tsx
+++ src/components/ArticlesList.tsx
@@ -1,60 +1,147 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Pagination, Button } from '@/components/';
-import { articles as allArticles } from '@/constants/';
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Pagination, Button, Search } from "@/components/";
+import { format } from "date-fns";
+import NoResultsPage from "@/components/NoResult";
+import Loader from "@/components/Loader";
+import { fetchArticles } from '@/constants/fetchArticles';
+import { truncateExcerpt } from "@/constants/truncatedText";
 
 interface ArticlesListProps {
-    articles: typeof allArticles;
-    currentPage?: number;
-    totalPages?: number;
-    onPageChange?: (page: number) => void;
-    showPagination?: boolean;
+  articles: Article[];
+  currentPage?: number;
+  onPageChange?: (page: number) => void;
+  showPagination?: boolean;
+  totalPages?: number;
+}
+
+interface ImageData {
+  data: {
+    id: number;
+    attributes: {
+      name: string;
+      alternativeText: string;
+      width: number;
+      height: number;
+      url: string;
+    };
+  };
+}
+
+interface Article {
+  id: number;
+  title: string;
+  author: string;
+  excerpt: any[];
+  image: ImageData;
+  link: string;
+  editorPick: boolean;
+  publishedAt: string;
 }
 
 const ArticlesList: React.FC<ArticlesListProps> = ({
-    articles,
-    currentPage = 1,
-    totalPages = 1,
-    onPageChange = () => {},
-    showPagination = true
+  currentPage = 1,
+  onPageChange = () => {},
+  showPagination = true,
 }) => {
-    return (
-        <div className="space-y-6">
-            <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 my-8">
-                {articles.map((article) => (
-                    <ArticleCard key={article.id} article={article} />
-                ))}
-            </section>
-            {showPagination && (
-                <Pagination
-                    currentPage={currentPage}
-                    totalPages={totalPages}
-                    onPageChange={onPageChange}
-                />
-            )}
-        </div>
-    );
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [totalArticles, setTotalArticles] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const articlesPerPage = 3;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const loadArticles = async () => {
+      setLoading(true);
+      const { articles, totalPages } = await fetchArticles({
+        page: currentPage,
+        pageSize: articlesPerPage,
+        searchQuery,
+      });
+      setArticles(articles);
+      setTotalPages(totalPages);
+      setLoading(false);
+    };
+
+    loadArticles();
+  }, [currentPage, searchQuery]);
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+    onPageChange(1);
+  };
+
+  return (
+    <div className="space-y-6">
+      <Search showAllPublicationsLink={false} onSearch={handleSearch} />
+      {loading ? (
+        <Loader />
+      ) : articles.length === 0 ? (
+        <NoResultsPage />
+      ) : (
+        <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 my-8">
+          {articles.map((article) => (
+            <ArticleCard key={article.id} article={article} />
+          ))}
+        </section>
+      )}
+      {showPagination && !loading && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={onPageChange}
+        />
+      )}
+    </div>
+  );
 };
 
-const ArticleCard: React.FC<{ article: typeof allArticles[0] }> = ({ article }) => {
-    return (
-        <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow">
-            <div className="relative w-full h-48">
-                <Image src={article.image} alt={article.title} layout="fill" objectFit="cover" />
-            </div>
-            <div className="p-6">
-                <h2 className="text-lg font-semibold text-gray-900 mb-2">{article.title}</h2>
-                <p className="text-sm text-gray-600 mb-4">by {article.author}</p>
-                <p className="text-sm text-gray-700 mb-4">{article.excerpt}</p>
-                <Link href={article.link}>
-                    <Button>
-                        Read more
-                    </Button>
-                </Link>
-            </div>
-        </div>
-    );
+const ArticleCard: React.FC<{ article: Article }> = ({ article }) => {
+  const truncatedExcerpt = truncateExcerpt(article.excerpt, 500);
+
+  return (
+    <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow">
+      <div className="relative w-full h-48">
+        <Image
+          src={`${process.env.NEXT_PUBLIC_STRAPI}${article.image}`}
+          alt={article.title}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="p-6">
+        <h2 className="text-lg font-semibold text-gray-900 mb-2">
+          {article.title}
+        </h2>
+        <p className="text-sm text-gray-600 mb-4">
+          by {article.author} on{" "}
+          <span className="text-sm text-gray-600">
+            {" "}
+            {format(new Date(article.publishedAt), "MMMM d, yyyy, h:mm a")}
+          </span>
+        </p>
+        <p>
+          {truncatedExcerpt.map(
+            (
+              paragraph: { children: any[] },
+              index: React.Key | null | undefined
+            ) => (
+              <p key={index} className="text-sm text-gray-700 mb-4">
+                {paragraph.children.map(
+                  (child: any, childIndex: number) => child.text
+                )}
+              </p>
+            )
+          )}{"....."}
+        </p>
+        <Link href={article.link}>
+          <Button>Read more</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ArticlesList;
