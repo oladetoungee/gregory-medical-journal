e183e533d6697925c95d7a3795618c42a0bcc8a5
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-08-30 00:13:00 +0100

    added the rest of the data

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,50 +1,79 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { PlusIcon, MinusIcon } from "@radix-ui/react-icons";
+import { Typewriter } from "@/components";
 
-'use client';
-
-import { useState } from 'react';
-import { PlusIcon, MinusIcon } from '@radix-ui/react-icons'; 
-import { Typewriter } from '@/components';
-import faq from '@/constants/faq';
+interface FAQs {
+  id: number;
+  question: string;
+  answer: string;
+}
 
 const FAQ = () => {
-    const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const [faqs, setFaqs] = useState<FAQs[]>([]);
 
-    const toggleAnswer = (index: number) => {
-        setOpenIndex(openIndex === index ? null : index);
+  useEffect(() => {
+    const fetchFaqs = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.NEXT_PUBLIC_STRAPI_URL}/faqs`
+        );
+
+        const faqs = response.data.data.map((data: any) => ({
+          id: data.id,
+          question: data.attributes.question,
+          answer: data.attributes.answer,
+        }));
+
+        setFaqs(faqs);
+      } catch (error) {
+        console.log("error getting faqs:", error);
+      }
     };
 
-    return (
-        <div className="mt-12 py-12 px-6 sm:px-12 lg:px-12 bg-white">
-            <div className="container mx-auto space-y-8">
-            <Typewriter
-                    text='Frequently Asked Questions'
-                    className="page-header"
-                />
-               
-                {faq.map((item, index) => (
-                    <div key={index} className="space-y-4">
-                        <div 
-                            className="flex justify-between items-center cursor-pointer"
-                            onClick={() => toggleAnswer(index)}
-                        >
-                            <h2 className="text-base font-semibold text-gray-500">{item.question}</h2>
-                            <button className="focus:outline-none">
-                                {openIndex === index ? (
-                                    <MinusIcon className="h-5 w-5 text-primary" />
-                                ) : (
-                                    <PlusIcon className="h-5 w-5 text-primary" />
-                                )}
-                            </button>
-                        </div>
-                        {openIndex === index && (
-                            <p className="text-primary text-sm mt-4 opacity-60">{item.answer}</p>
-                        )}
-                        <hr className="border-t border-gray-300 my-4" />
-                    </div>
-                ))}
+    fetchFaqs();
+  }),
+    [];
+
+  const toggleAnswer = (index: number) => {
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
+  return (
+    <div className="mt-12 py-12 px-6 sm:px-12 lg:px-12 bg-white">
+      <div className="container mx-auto space-y-8">
+        <Typewriter text="Frequently Asked Questions" className="page-header" />
+
+        {faqs.map((faq) => (
+          <div key={faq.id} className="space-y-4">
+            <div
+              className="flex justify-between items-center cursor-pointer"
+              onClick={() => toggleAnswer(faq.id)}
+            >
+              <h2 className="text-base font-semibold text-gray-500">
+                {faq.question}
+              </h2>
+              <button className="focus:outline-none">
+                {openIndex === faq.id ? (
+                  <MinusIcon className="h-5 w-5 text-primary" />
+                ) : (
+                  <PlusIcon className="h-5 w-5 text-primary" />
+                )}
+              </button>
             </div>
-        </div>
-    );
+            {openIndex === faq.id && (
+              <p className="text-primary text-sm mt-4 opacity-60">
+                {faq.answer}
+              </p>
+            )}
+            <hr className="border-t border-gray-300 my-4" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default FAQ;
--- src/components/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy.tsx
@@ -1,28 +1,60 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Typewriter } from "@/components";
 
-'use client';
-
-import privacyPolicy from '@/constants/privacyPolicy';
-import { Typewriter } from '@/components';
+interface Policies {
+  id: number;
+  policy: string;
+  desc: string;
+}
 
 const PrivacyPolicy = () => {
-    return (
-        <div className="mt-12 py-12 px-6 sm:px-12 lg:px-12 bg-white">
-            <div className="container mx-auto space-y-8">
-                <Typewriter
-                    text={privacyPolicy.title}
-                    className="page-header"
-                />
-                <p className="md:text-base tex-sm text-sec">{privacyPolicy.content}</p>
-                {privacyPolicy.sections.map((section, index) => (
-                    <div key={index} className="space-y-4 mt-6">
-                        <h2 className="md:text-xl text-base font-semibold text-primary">{section.heading}</h2>
-                        <p className="text-sec">{section.text}</p>
-                        <hr className="border-t border-gray-300 my-4" />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  const [policies, setPolicies] = useState<Policies[]>([]);
+
+  useEffect(() => {
+    const fetchPolicies = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_STRAPI_URL}/policies`
+        );
+
+        const policies = res.data.data.map((data: any) => ({
+          id: data.id,
+          policy: data.attributes.policy,
+          desc: data.attributes.desc,
+        }));
+
+        setPolicies(policies);
+      } catch (error) {
+        console.log("error getting policies:", error);
+      }
+    };
+
+    fetchPolicies();
+  }, []);
+
+  return (
+    <div className="mt-12 py-12 px-6 sm:px-12 lg:px-12 bg-white">
+      <div className="container mx-auto space-y-8">
+        <Typewriter text="Privacy Policy" className="page-header" />
+        <p className="md:text-base tex-sm text-sec">
+          Gregory Medical Journal respects your privacy and is committed to
+          protecting your personal information. This policy outlines how we
+          collect, use, and safeguard your data.
+        </p>
+        {policies.map((policy) => (
+          <div key={policy.id} className="space-y-4 mt-6">
+            <h2 className="md:text-xl text-base font-semibold text-primary">
+              {policy.policy}
+            </h2>
+            <p className="text-sec">{policy.desc}</p>
+            <hr className="border-t border-gray-300 my-4" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PrivacyPolicy;
