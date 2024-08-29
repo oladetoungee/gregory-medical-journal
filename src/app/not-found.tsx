350da7588a6fd36d537c89cb54b70136d1456cc2
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-08-29 19:48:31 +0100

    fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ const NotFoundPage = () => {
         <>
           <Header />
           <main className="flex flex-col items-center justify-center h-screen text-center p-4">
-          <h1 className="text-4xl font-bold mb-4">This Page Is Still Being Worked On</h1>
+            <h1 className="text-4xl font-bold mb-4">This Page Is Still Being Worked On</h1>
             <p className="text-lg mb-6">In due time, Gbemi will finish cooking</p>
             <a href="/" className="text-blue-500 underline">Go back to Home</a>
           </main>
