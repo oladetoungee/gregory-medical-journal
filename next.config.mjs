30f65dfb292a448039287d27f848e912109200f3
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-08-29 02:34:09 +0100

    imgs fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', ''],
+    domains: ['localhost', 'gregory-journal-backend.onrender.com'],
   },
 };
 
