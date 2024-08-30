660d9a5cac629ebd1a898626a5615439e379f967
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-08-30 01:58:05 +0100

    dynamic pages

---
+++ src/app/journals/articles/[id]/page.tsx
@@ -0,0 +1,239 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { format } from "date-fns";
+import axios from 'axios';
+import { truncateExcerpt } from "@/constants/truncatedText";
+import Loader from "@/components/Loader";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/";
+
+interface ImageData {
+  id: number;
+  attributes: {
+    name: string;
+    alternativeText: string;
+    width: number;
+    height: number;
+    url: string;
+  };
+}
+
+interface Article {
+  id: number;
+  title: string;
+  author: string;
+  excerpt: any[];
+  image?: ImageData;
+  link: string;
+  editorPick: boolean;
+  publishedAt: string;
+}
+
+const Page: React.FC = () => {
+  const [article, setArticle] = useState<Article | null>(null);
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+
+  useEffect(() => {
+    const fetchArticle = async () => {
+      if (!id) return;
+      try {
+        const res = await axios.get(`${process.env.NEXT_PUBLIC_STRAPI_URL}/articles/${id}?populate=*`);
+        const articleData = res.data?.data?.attributes;
+        if (articleData) {
+          setArticle({ id: res.data.data.id, ...articleData });
+          console.log(articleData);
+        } else {
+          console.error("Article data is not available");
+        }
+      } catch (error) {
+        console.error("Error fetching article:", error);
+      }
+    };
+    
+    fetchArticle();
+  }, [id]);
+
+  if (!article) {
+    return <Loader />;
+  }
+
+  const truncatedExcerpt = truncateExcerpt(article.excerpt, 2000);
+
+  const imageUrl = article.image?.attributes?.url
+    ? `${process.env.NEXT_PUBLIC_STRAPI}${article.image.attributes.url}`
+    : "/default-image.png"; // Fallback to a default image if URL is not available
+
+  const imageAlt = article.image?.attributes?.alternativeText || article.title;
+
+  return (
+    <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow mt-6">
+      {article.image && (
+        <div className="relative w-full h-48">
+          <Image
+            src={imageUrl}
+            alt={imageAlt}
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+      )}
+      <div className="p-6">
+        <h2 className="text-lg font-semibold text-gray-900 mb-2">
+          {article.title}
+        </h2>
+        <p className="text-sm text-gray-600 mb-4">
+          by {article.author} on{" "}
+          <span className="text-sm text-gray-600">
+            {format(new Date(article.publishedAt), "MMMM d, yyyy, h:mm a")}
+          </span>
+        </p>
+        <div>
+          {truncatedExcerpt.map(
+            (paragraph: { children: any[] }, index: React.Key) => (
+              <p key={index} className="text-sm text-gray-700 mb-4">
+                {paragraph.children.map(
+                  (child: any, childIndex: number) => child.text
+                )}
+              </p>
+            )
+          )}
+          {"....."}
+        </div>
+        <Link href={article.link}>
+          <Button>Read more</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
+
+
+
+
+
+// "use client";
+// import React, { useEffect, useState } from "react";
+// import { useSearchParams } from "next/navigation";
+// import { format } from "date-fns";
+// import axios from 'axios';
+// import { truncateExcerpt } from "@/constants/truncatedText";
+// import Loader from "@/components/Loader";
+// import Image from "next/image";
+// import Link from "next/link";
+// import { Button } from "@/components/";
+
+// interface ImageData {
+//   id: number;
+//   attributes: {
+//     name: string;
+//     alternativeText: string;
+//     width: number;
+//     height: number;
+//     url: string;
+//   };
+// }
+
+// interface Article {
+//   id: number;
+//   title: string;
+//   author: string;
+//   excerpt: any[];
+//   image?: ImageData;
+//   link: string;
+//   editorPick: boolean;
+//   publishedAt: string;
+// }
+
+// const Page: React.FC = () => {
+//   const [article, setArticle] = useState<Article | null>(null);
+//   const [error, setError] = useState<string | null>(null);
+//   const searchParams = useSearchParams();
+//   const id = searchParams.get("id");
+
+//   useEffect(() => {
+//     const fetchArticle = async () => {
+//       try {
+//         const res = await axios.get(`${process.env.NEXT_PUBLIC_STRAPI_URL}/articles/${id}?populate=image`);
+//         const articleData = res.data.data?.attributes;
+
+//         if (articleData) {
+//           setArticle({
+//             id: res.data.data.id,
+//             title: articleData.title,
+//             author: articleData.author,
+//             excerpt: articleData.excerpt,
+//             image: articleData.image?.data?.attributes || null,
+//             link: articleData.link,
+//             editorPick: articleData.editorPick,
+//             publishedAt: articleData.publishedAt,
+//           });
+//         }
+//       } catch (error) {
+//         console.error("Error fetching article:", error);
+//         setError("Failed to fetch article data. Please try again later.");
+//       }
+//     };
+
+//     if (id) {
+//       fetchArticle();
+//     }
+//   }, [id]);
+
+//   if (error) {
+//     return <div>{error}</div>;
+//   }
+
+//   if (!article) {
+//     return <Loader />;
+//   }
+
+//   const truncatedExcerpt = truncateExcerpt(article.excerpt, 2000);
+
+//   return (
+//     <div className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-lg transition-shadow">
+//       {article.image && (
+//         <div className="relative w-full h-48">
+//           <Image
+//             src={`${process.env.NEXT_PUBLIC_STRAPI}${article.image?.attributes.url}`}
+//             alt={article.image?.attributes.alternativeText || article.title}
+//             layout="fill"
+//             objectFit="cover"
+//           />
+//         </div>
+//       )}
+//       <div className="p-6">
+//         <h2 className="text-lg font-semibold text-gray-900 mb-2">
+//           {article.title}
+//         </h2>
+//         <p className="text-sm text-gray-600 mb-4">
+//           by {article.author} on{" "}
+//           <span className="text-sm text-gray-600">
+//             {format(new Date(article.publishedAt), "MMMM d, yyyy, h:mm a")}
+//           </span>
+//         </p>
+//         <div>
+//           {truncatedExcerpt.map(
+//             (paragraph: { children: any[] }, index: React.Key) => (
+//               <p key={index} className="text-sm text-gray-700 mb-4">
+//                 {paragraph.children?.map(
+//                   (child: any, childIndex: number) => child.text
+//                 )}
+//               </p>
+//             )
+//           )}
+//           {"....."}
+//         </div>
+//         <Link href={article.link}>
+//           <Button>Read more</Button>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Page;
--- src/components/TermsOfUse.tsx
+++ src/components/TermsOfUse.tsx
@@ -1,60 +1,61 @@
-'use client';
+"use client";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import termsOfUse from '@/constants/termsOfUse';
-import { Typewriter } from '@/components';
+import termsOfUse from "@/constants/termsOfUse";
+import { Typewriter } from "@/components";
 
 interface TermsOfUse {
-    id: number;
-    term: string;
-    desc: string;
-  }
-  
+  id: number;
+  term: string;
+  desc: string;
+}
+
 const TermsOfUse = () => {
-    const [terms, setTerms] = useState<TermsOfUse[]>([]);
+  const [terms, setTerms] = useState<TermsOfUse[]>([]);
 
-    useEffect(() => {
-        const fetchTerms = async () => {
-            try {
-                const res = await axios.get(
-                    `${process.env.NEXT_PUBLIC_STRAPI_URL}/term-of-uses`
-                  );
+  useEffect(() => {
+    const fetchTerms = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_STRAPI_URL}/term-of-uses`
+        );
 
-                const termsOfUses = res.data.data.map((data: any) => ({
-                    id: data.id,
-                    term: data.attributes.term,
-                    desc: data.attributes.desc
-                }));
+        const termsOfUses = res.data.data.map((data: any) => ({
+          id: data.id,
+          term: data.attributes.term,
+          desc: data.attributes.desc,
+        }));
 
-                setTerms(termsOfUses);
-            } catch (error) {
-                console.log('error getting terms:', error);
-            }
-        };
+        setTerms(termsOfUses);
+      } catch (error) {
+        console.log("error getting terms:", error);
+      }
+    };
 
-        fetchTerms();
-    }, []);
+    fetchTerms();
+  }, []);
 
-    return (
-        <div className="mt-12 py-12 px-6 sm:px-12 lg:px-12 bg-white">
-            <div className="container mx-auto space-y-8">
-                <Typewriter
-                    text="Terms of Use"
-                    className="page-header"
-                />
-                <p className="md:text-base tex-sm text-sec">
-                By using Gregory Medical Journal's website, you agree to comply with these terms of use. Please read them carefully before accessing our services.
-                </p>
-                {terms.map((term) => (
-                    <div key={term.id} className="space-y-4 mt-2">
-                        <h2 className="md:text-xl text-base font-semibold text-primary">{term.term}</h2>
-                        <p className="text-sec">{term.desc}</p>
-                        <hr className="border-t border-gray-300 my-2" />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="mt-12 py-12 px-6 sm:px-12 lg:px-12 bg-white">
+      <div className="container mx-auto space-y-8">
+        <Typewriter text="Terms of Use" className="page-header" />
+        <p className="md:text-base tex-sm text-sec">
+          By using Gregory Medical Journal's website, you agree to comply with
+          these terms of use. Please read them carefully before accessing our
+          services.
+        </p>
+        {terms.map((term) => (
+          <div key={term.id} className="space-y-4 mt-2">
+            <h2 className="md:text-xl text-base font-semibold text-primary">
+              {term.term}
+            </h2>
+            <p className="text-sec">{term.desc}</p>
+            <hr className="border-t border-gray-300 my-2" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TermsOfUse;
