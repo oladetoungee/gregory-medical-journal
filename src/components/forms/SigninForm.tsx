09c6dbda20dd44b0c18b668bfa1bc31ea60ec51d
oladetoungee <demilad1998@gmail.com>
2024-09-02 21:55:59 +0100

    auth comnpleted

--- src/components/forms/SigninForm.tsx
+++ src/components/forms/SigninForm.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Link from "next/link";
-
+import { useFormState } from "react-dom";
+import { loginUserAction } from "@/data/actions/auth-actions";
 import {
   CardTitle,
   CardDescription,
@@ -14,11 +15,24 @@ import {
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import TermsAndPolicy from "./TermsAndPolicy";
+import { StrapiErrors } from "@/components/forms/StrapiErrors";
+import { SubmitButton } from "@/components/ui/SubmitButton";
+import { ZodErrors } from "./ZodErrors";
+
+const INITIAL_STATE = {
+  zodErrors: null,
+  strapiErrors: null,
+  data: null,
+  message: null,
+};
 
 export default function SigninForm() {
+  const [formState, formAction] = useFormState(loginUserAction, INITIAL_STATE);
+
+
   return (
     <div className="w-full max-w-md text-gray-400 ">
-      <form>
+   <form action={formAction}>
         <Card>
           <CardHeader className="space-y-1">
             <CardTitle className="text-2xl text-white mb-2">Welcome Back!</CardTitle>
@@ -35,6 +49,7 @@ export default function SigninForm() {
                 type="text"
                 placeholder="username or email"
               />
+              <ZodErrors error={formState?.zodErrors?.identifier} />
             </div>
             <div className="space-y-2">
               <Label htmlFor="password">Password</Label>
@@ -44,10 +59,15 @@ export default function SigninForm() {
                 type="password"
                 placeholder="password"
               />
+               <ZodErrors error={formState.zodErrors?.password} />
             </div>
           </CardContent>
-          <CardFooter className="flex flex-col">
-            <button className="w-full">Sign In</button>
+          <CardFooter className="flex flex-col items-center">
+          <SubmitButton className="w-full flex justify-center items-center text-white" 
+              text="Sign In"
+              loadingText="Loading"
+            />
+            <StrapiErrors error={formState?.strapiErrors} />
           </CardFooter>
         </Card>
         <div className="mt-4 text-center text-sm">
--- src/data/actions/auth-actions.ts
+++ src/data/actions/auth-actions.ts
@@ -1,64 +1,143 @@
-'use server'
+"use server";
+import { z } from "zod";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { z, ZodError } from 'zod';
-import { registerUserService } from '../services/auth-service';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-
+import {
+  registerUserService,
+  loginUserService,
+} from "@/data/services/auth-service";
 
 const config = {
-    maxAge: 60 * 60 * 24 * 7, // 1 week
-    path: "/",
-    domain: process.env.HOST ?? "localhost",
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-  };
+  maxAge: 60 * 60 * 24 * 7, // 1 week
+  path: "/",
+  domain: process.env.HOST ?? "localhost",
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+};
 
-const schemaRegisterUser = z.object({
-    username: z.string().min(3).max(20, {
-        message: 'Username must be between 3 and 20 characters',
-    }),
-    email: z.string().email({
-        message: 'Invalid email address',
-    }),
-    password: z.string().min(6).max(25, {
-        message: 'Password must be between 6 and 25 characters',
-    }),
-    });
+const schemaRegister = z.object({
+  username: z.string().min(3).max(20, {
+    message: "Username must be between 3 and 20 characters",
+  }),
+  password: z.string().min(6).max(100, {
+    message: "Password must be between 6 and 100 characters",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address",
+  }),
+});
 
-export  async function registerUserAction(prevState: any, formData: FormData) {
+export async function registerUserAction(prevState: any, formData: FormData) {
+  const validatedFields = schemaRegister.safeParse({
+    username: formData.get("username"),
+    password: formData.get("password"),
+    email: formData.get("email"),
+  });
 
- const validateFields = schemaRegisterUser.safeParse({
-        username: formData.get('username') as string,
-        email: formData.get('email') as string,
-        password: formData.get('password') as string,
-    });
-if (!validateFields.success) {
-        return {
-            ...prevState,
-          zodErrors: validateFields.error.flatten().fieldErrors,
-            message: 'Invalid form data',
-        };
-    }
+  if (!validatedFields.success) {
+    return {
+      ...prevState,
+      zodErrors: validatedFields.error.flatten().fieldErrors,
+      strapiErrors: null,
+      message: "Missing Fields. Failed to Register.",
+    };
+  }
 
-    const responseData = await registerUserService(validateFields.data);
+  const responseData = await registerUserService(validatedFields.data);
 
-if (responseData.error) {
-        return {
-            ...prevState,
-            strapiErrors: responseData.error,
-            ZodErrors: null,
-            message: responseData.message,
-        };
-    }
+  if (!responseData) {
+    return {
+      ...prevState,
+      strapiErrors: null,
+      zodErrors: null,
+      message: "Ops! Something went wrong. Please try again.",
+    };
+  }
 
-    cookies().set("jwt", responseData.jwt, config);
-    redirect("/dashboard");
-    console.log("responseData", responseData.jwt);
-return {
-    ...prevState,
-    data: "User registered successfully",
+  if (responseData.error) {
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      message: "Failed to Register.",
+    };
+  }
+
+  cookies().set("jwt", responseData.jwt, config);
+  redirect("/dashboard");
 }
 
- 
+const schemaLogin = z.object({
+    identifier: z
+      .string()
+      .min(3, {
+        message: "Identifier must have at least 3 or more characters",
+      })
+      .max(100)
+      .refine((value) => {
+        return (
+          /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) || // Checks if it's a valid email
+          /^[a-zA-Z0-9_]+$/.test(value)              // Checks if it's a valid username
+        );
+      }, {
+        message: "Please enter a valid username or email address",
+      }),
+    password: z
+      .string()
+      .min(6, {
+        message: "Password must have at least 6 or more characters",
+      })
+      .max(100, {
+        message: "Password must be between 6 and 100 characters",
+      }),
+  });
+  
+export async function loginUserAction(prevState: any, formData: FormData) {
+    const validatedFields = schemaLogin.safeParse({
+      identifier: formData.get("identifier"),
+      password: formData.get("password"),
+    });
+  
+    if (!validatedFields.success) {
+      console.log("Validation Errors:", validatedFields.error.flatten().fieldErrors);
+      return {
+        ...prevState,
+        zodErrors: validatedFields.error.flatten().fieldErrors,
+        message: "Missing Fields. Failed to Login.",
+      };
     }
+  
+    const responseData = await loginUserService(validatedFields.data);
+  
+    if (!responseData) {
+      console.log("No response data from loginUserService");
+      return {
+        ...prevState,
+        strapiErrors: responseData.error,
+        zodErrors: null,
+        message: "Ops! Something went wrong. Please try again.",
+      };
+    }
+  
+    if (responseData.error) {
+      console.log("Strapi Errors:", responseData.error);
+      return {
+        ...prevState,
+        strapiErrors: responseData.error,
+        zodErrors: null,
+        message: "Failed to Login.",
+      };
+    }
+  
+    cookies().set("jwt", responseData.jwt, config);
+  
+    console.log("Redirecting to dashboard...");
+    redirect("/dashboard");
+  }
+  
+
+export async function logoutAction() {
+  cookies().set("jwt", "", { ...config, maxAge: 0 });
+  redirect("/");
+}
